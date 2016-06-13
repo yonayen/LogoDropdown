f0e89c76163fd366751b5a3f8007217260d0e750
Yonathan Ayenew <yonayen@gmail.com>
2016-06-12 23:12:53 -0700

    change src attribute for logo dropdown

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 $(function(){
 	
+0
+down vote
+If I understood your question right, try this:
+
+Let's say your HTML is something like this:
+
+<img class="product_image" src="first.png">
+You can get the attr() with jQuery like this:
+
+$(".product_image").attr("src","second.png");
+and then using your buttons you load a click function like:
+
+$('yourButtons').click(function() {
+    $('.product_image').attr('src','second.jpg');
+});
 });

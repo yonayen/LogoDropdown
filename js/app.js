20d04b0f6ccf68151fe18a18caee693dd1d290c0
Yonathan Ayenew <yonayen@gmail.com>
2016-06-13 23:55:23 -0700

    jQuery

--- js/app.js
+++ js/app.js
@@ -1,10 +1,4 @@
 $(function(){
-	
-0
-down vote
-If I understood your question right, try this:
-
-Let's say your HTML is something like this:
 
 <img class="product_image" src="first.png">
 You can get the attr() with jQuery like this:

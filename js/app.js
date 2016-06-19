ad5910cdd1c21e9ae32d30fe305e15343c852a52
Yonathan Ayenew <yonayen@gmail.com>
2016-06-18 23:23:26 -0700

    remove jQuery code

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-$(function(){
-
-<img class="product_image" src="first.png">
-You can get the attr() with jQuery like this:
-
-$(".product_image").attr("src","second.png");
-and then using your buttons you load a click function like:
-
-$('yourButtons').click(function() {
-    $('.product_image').attr('src','second.jpg');
-});
-});
 
 $function()
 {

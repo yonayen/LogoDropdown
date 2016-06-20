7ac2cda1aa0968d0984062fd23d2a385c2dae59b
Yonathan Ayenew <yonayen@gmail.com>
2016-06-20 14:14:57 -0700

    comments added

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
 
-$function(){
-	$(".color").hide();
-	$
-};
+
 $function()
 {
 	$(".color").hide();
 	$(".grayscale").show();
-​
+
 	$(".filter").on("click",function()
 	{
 		$(".color").hide();
 		$(".grayscale").hide();
 		$(".color." + $(this).data("filter")).show();
 	});
-​
-});
-​
+
+}
+
+/*
+
+	.css( "opacity", 1 )
+    .fadeIn( 30 )
+    .fadeOut( 1000 );
+*/
+
+// on click 'value of data-filter' show color and hide grayscale 
+// of items with matching filter
+
+// on 

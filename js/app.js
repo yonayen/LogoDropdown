bb9dce528e1176965505ae15a8c2e5eebfa61fe7
Yonathan Ayenew <yonayen@gmail.com>
2016-11-04 15:11:14 -0700

    fix logoDropdown

--- js/app.js
+++ js/app.js
@@ -16,16 +16,16 @@ $function()
 
 function()
 {
-	
+
 }
-/*
+
 
 	.css( "opacity", 1 )
     .fadeIn( 30 )
     .fadeOut( 1000 );
-*/
 
-// on click 'value of data-filter' show color and hide grayscale 
+
+// on click 'value of data-filter' show color and hide grayscale
 // of items with matching filter
 
-// on 
+// on
